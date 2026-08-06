91db12fa0504d5fdd9e56c6294d6365d42e5a4f9
Vinicius-de-Castro <viniciuscastro0711@gmail.com>
2026-08-06 10:31:22 -0300

    Initial Commit

---
+++ AuraFarmer/AuraFarmerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraFarmerApp.swift
+//  AuraFarmer
+//
+//  Created by Vinicius Rodrigues de Castro on 06/08/26.
+//
+
+import SwiftUI
+
+@main
+struct AuraFarmerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
